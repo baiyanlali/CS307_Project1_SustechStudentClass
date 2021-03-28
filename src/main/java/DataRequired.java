857a0e71122fa9c9b336c7c2a8326ca677b1da9c
baiyanlali <zao44457660@126.com>
2021-03-28 16:33:01 +0800

    Initial Commit

---
+++ src/main/java/DataRequired.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class DataRequired {
+
+    public static void main(String[] args) {
+        HashSet<Teacher> courses=new HashSet<>();
+        Teacher t1=new Teacher(0,"A");
+        Teacher t2=new Teacher(1,"A");
+        courses.add(t1);
+        courses.add(t2);
+
+    }
+
+}
+
+class Course{
+    String course_id;
+    int total_capacity;
+    String course_name;
+    int course_hour;
+    float course_credit;
+    String course_departure;
+
+    Course(String course_id,int total_capacity,String course_name,int course_hour,float course_credit,String course_departure){
+        this.course_id=course_id;
+        this.total_capacity=total_capacity;
+        this.course_name=course_name;
+        this.course_hour=course_hour;
+        this.course_credit=course_credit;
+        this.course_departure=course_departure;
+        this.classes=new ArrayList<>();
+    }
+
+    //引用
+    List<Class> classes;
+}
+
+class Class{
+    String class_id;
+    String class_name;
+
+    Class(String class_name,Course course){
+        this.class_name=class_name;
+        this.class_id= course.course_id+class_name;
+        this.course=course;
+        class_info_list=new ArrayList<>();
+        teachers=new ArrayList<>();
+        course.classes.add(this);
+    }
+
+    //引用
+    List<ClassList> class_info_list;
+    Course course;
+    List<Teacher> teachers;
+}
+
+class Teacher{
+    int teacher_id;
+    String names;
+    @Override
+    public boolean equals(Object obj){
+        Teacher other=(Teacher) obj;
+        if(other==null)return false;
+        if(/*other.teacher_id!=this.teacher_id || */!other.names.equals(this.names))return false;
+        return true ;
+    }
+    Teacher(int teacher_id,String name){
+        this.teacher_id=teacher_id;
+        this.names=name;
+    }
+    Teacher(String name){
+        this.names=name;
+    }
+    @Override
+    public int hashCode() {
+//        return teacher_id+names.hashCode();
+        return names.hashCode();
+    }
+}
+//为了能通过class找到class_list, 将 classlist的引用放在class中，而不是像数据库那样反过来
+class ClassList{
+    int[] week_list;
+    int week_day;
+    String location;
+    int start_time;
+    int end_time;
+    ClassList(ClassListRAW clw){
+        week_list=clw.weekList;
+        week_day= clw.weekday;
+        location=clw.location;
+        String[] class_time=clw.classTime.split("-");
+        start_time=Integer.parseInt(class_time[0]);
+        end_time=Integer.parseInt(class_time[1]);
+    }
+
+}
+
+class Location{
+    String name;
+}
+
+//TODO: the most important and difficult content
+class Prerequisite{
+    Course now_course;//当前的course
+    long id;//当前语句
+
+}
---
+++ src/main/java/JwxtParser.java
@@ -0,0 +1,89 @@
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+// TODO: import the json library of your choice
+
+public class JwxtParser {
+    public static List<CourseRAW> courses;
+    // Run as: jshell JwxtParser.java <json file>
+    public static void main(String[] args) throws IOException {
+//        Path path = Path.of("src/main/java/data/course_info.json");
+        Path path = Path.of(args[0]);
+        System.out.println(path.isAbsolute());
+        System.out.println(path.getRoot());
+        String content = Files.readString(path);
+        Gson gson = new Gson();
+        courses = gson.fromJson(content, new TypeToken<List<CourseRAW>>(){}.getType());
+        parseCourseRAW();
+    }
+
+    public static void parseCourseRAW(){
+        HashMap<String,Course> courseHashMap=new HashMap<>();
+        ArrayList<Class> classes=new ArrayList<>();
+        HashSet<Teacher> teachers=new HashSet<>();
+        int teacher_index=0;
+        HashSet<Location> locations=new HashSet<>();
+        for (CourseRAW course_raw:courses) {
+            //Course info_去重
+            if(!courseHashMap.containsKey(course_raw.courseName.trim())){
+                Course co=new Course(   course_raw.courseId.trim(),
+                        course_raw.totalCapacity,
+                        course_raw.courseName.trim(),
+                        course_raw.courseHour,
+                        course_raw.courseCredit,
+                        course_raw.courseDept);
+                courseHashMap.put(co.course_name,co);
+            }
+            //Class info 不需要去重
+            Class clAss=new Class(course_raw.className.trim(), courseHashMap.get(course_raw.courseName.trim()));
+//            clAss.class_info_list.addAll(course_raw.classList);
+            for (ClassListRAW cl:course_raw.classList) {
+                ClassList classList=new ClassList(cl);
+                clAss.class_info_list.add(classList);
+            }
+            String[] manyTeacher = course_raw.teacher.split(",");
+            for (String oneTeacher:manyTeacher) {
+
+                Teacher teacher=new Teacher(oneTeacher);
+                if(!teachers.contains(teacher)){
+                    teachers.add(teacher);
+                }
+                clAss.teachers.add(teacher);
+            }
+
+        }
+    }
+
+}
+
+class CourseRAW {
+    // TODO:
+    public int totalCapacity;
+    public String courseId;
+    public String prerequisite;
+    public String teacher;
+    public ClassListRAW[] classList;
+    public int courseHour;
+    public float courseCredit;
+    public String courseName;
+    public String courseDept;
+    public String className;
+}
+
+class ClassListRAW {
+    // TODO: define data-class as the json structure
+    public int[] weekList;
+    public String location;
+    public String classTime;
+    public int weekday;
+
+}
+
+
